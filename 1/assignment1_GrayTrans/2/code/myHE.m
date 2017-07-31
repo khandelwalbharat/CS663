8b3d22bd47cd819eb883099d0d3c278879298e12
rohitrango <rohit.rango@gmail.com>
2017-07-31 19:59:48 +0530

    myHE bug correction

--- 1/assignment1_GrayTrans/2/code/myHE.m
+++ 1/assignment1_GrayTrans/2/code/myHE.m
@@ -5,6 +5,6 @@ function [ new_img ] = myHE( img )
 img_vector = img(:);
 m_hist = imhist(img, 256);
 c_hist = 255*cumsum(m_hist)/sum(m_hist);
-new_img = reshape(c_hist(img_vector), m, n);
+new_img = reshape(c_hist(img_vector+1), m, n);
 end
 
--- 1/assignment1_GrayTrans/2/code/myMainScript.m
+++ 1/assignment1_GrayTrans/2/code/myMainScript.m
@@ -6,12 +6,15 @@ colorScale = 0:1/(myNumOfColors-1):1;
 myColorScale = [ colorScale' colorScale' colorScale' ];
 
 %% Your code here
-tic; 
 img1 = imread('../data/barbara.png');
 img2 = imread('../data/TEM.png');
 img3 = imread('../data/canyon.png');
 
 %% myHE
+tic;
 myHE1 = myHE(img1);
-
+imagesc(img1), colormap(myColorScale), colorbar;
+figure, imagesc(myHE1), colormap(myColorScale), colorbar;
 toc;
+
+
