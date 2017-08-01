09325f77867d480ec46ab6006b1bc92a750841eb
rohitrango <rohit.rango@gmail.com>
2017-08-01 21:15:34 +0530

    Be careful of uint8, Bilinear Interpolation fixed

--- 1/assignment1_GrayTrans/1/code/myBilinearInterpolation.m
+++ 1/assignment1_GrayTrans/1/code/myBilinearInterpolation.m
@@ -1,14 +1,14 @@
-function [new_img] = myBilinearInterpolation(img)
-    colormap(gray(256));
+function [new_img] = myBilinearInterpolation(iimg)
+    img = double(iimg);
     [m n] = size(img);
     index_rows = 1:3:(3*m-2);
     index_cols = 1:2:(2*n-1);
     new_img = zeros(3*m - 2, 2*n - 1);
     new_img(index_rows, index_cols) = img;
     % for rows
-    new_img(index_rows, 2:2:(2*n-1)) = (img(:,1:(n-1)) + img(:, 2:n)) / 2 ;
+    new_img(index_rows, 2:2:(2*n-1)) = (img(:,1:(n-1)) + img(:, 2:n)) / 2.0 ;
     % for cols
     index_rows_new = index_rows(1:((size(index_rows, 2)-1)));
-    new_img(index_rows_new+1, :) = (2*new_img(index_rows_new, :) + new_img(index_rows_new+3, :)) / 3;
-    new_img(index_rows_new+2, :) = (new_img(index_rows_new, :) + 2*new_img(index_rows_new+3, :)) / 3;
+    new_img(index_rows_new+1, :) = (2*new_img(index_rows_new, :) + new_img(index_rows_new+3, :)) / 3.0;
+    new_img(index_rows_new+2, :) = (new_img(index_rows_new, :) + 2*new_img(index_rows_new+3, :)) / 3.0;
 end
